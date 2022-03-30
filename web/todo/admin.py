cb3baf6110512e88a295120c10b43508116931b6
north-star-saj <bsajevic22@gmail.com>
2022-03-30 03:49:23 +0000

    WIP Update the todo app

--- web/todo/admin.py
+++ web/todo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from todo.models import (
+    ToDoItem,
+    ToDoList,
+)
 
 # Register your models here.
+
+admin.site.register(ToDoItem)
+admin.site.register(ToDoList)
--- web/todo/models.py
+++ web/todo/models.py
@@ -1,3 +1,46 @@
+# todo_list/todo_app/models.py
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
 
-# Create your models here.
+
+def one_week_hence():
+    """A thing"""
+    return timezone.now() + timezone.timedelta(days=7)
+
+
+class ToDoList(models.Model):
+    """A thing"""
+
+    title = models.CharField(max_length=100, unique=True)
+
+    def get_absolute_url(self):
+        """A thing"""
+        return reverse("list", args=[self.id])
+
+    def __str__(self):
+        """A thing"""
+        return str(self.title)
+
+
+class ToDoItem(models.Model):
+    """A thing"""
+
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(default=one_week_hence)
+    todo = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+
+    def get_absolute_url(self):
+        """A thing"""
+        return reverse("item-update", args=[str(self.todo.id), str(self.id)])
+
+    def __str__(self):
+        """A thing"""
+        return f"{self.title}: due {self.due_date}"
+
+    class Meta:
+        """A thing"""
+
+        ordering = ["due_date"]
